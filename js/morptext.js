4ce711ce8f4e739514a262c66f613ce290ec3d0d
cesarquispe20 <cesarquispe.20@gmail.com>
2020-11-23 17:53:34 -0500

    cambio de logo

---
+++ js/morptext.js
@@ -0,0 +1,316 @@
+$('#Mostrar').click(function(){
+
+    $('#titulouno').addClass('slideLeft');
+    
+       var nombre = $('#nombre1').val();
+       var cargo = $('#cargo1').val();
+
+       var txt = $("#inefect option:selected").text();
+       var val = $("#inefect option:selected").val();
+    
+    
+    $('#titulouno').html('<h1 class="card-title tlt text-dark" style="font-size: 2.4vw" > '+nombre+'</h1>');
+    textille(val, cargo);   
+
+});
+
+$('#Mostrar2').click(function(){
+
+    $('#titulouno').addClass('slideLeft');
+    
+       var nombre = $('#nombre2').val();
+       var cargo = $('#cargo2').val();
+
+       var txt = $("#inefect2 option:selected").text();
+       var val = $("#inefect2 option:selected").val();
+    
+    
+    $('#titulouno').html('<h1 class="card-title tlt text-dark"  style="font-size: 2.4vw"> '+nombre+'</h1>');   
+    textille(val, cargo);   
+    
+});
+$('#Mostrar3').click(function(){
+
+    $('#titulouno').addClass('slideLeft');
+    
+       var nombre = $('#nombre3').val();
+       var cargo = $('#cargo3').val();
+
+       var txt = $("#inefect3 option:selected").text();
+       var val = $("#inefect3 option:selected").val();
+    
+    
+    $('#titulouno').html('<h1 class="card-title tlt text-dark"  style="font-size: 2.4vw"> '+nombre+'</h1>');   
+    textille(val, cargo);   
+    
+});
+$('#Mostrar4').click(function(){
+
+    $('#titulouno').addClass('slideLeft');
+    
+       var nombre = $('#nombre4').val();
+       var cargo = $('#cargo4').val();
+
+       var txt = $("#inefect4 option:selected").text();
+       var val = $("#inefect4 option:selected").val();
+    
+    
+    $('#titulouno').html('<h1 class="card-title tlt text-dark"  style="font-size: 2.4vw"> '+nombre+'</h1>');   
+    textille(val, cargo);   
+    
+});
+$('#Mostrar5').click(function(){
+
+    $('#titulouno').addClass('slideLeft');
+    
+       var nombre = $('#nombre5').val();
+       var cargo = $('#cargo5').val();
+
+       var txt = $("#inefect5 option:selected").text();
+       var val = $("#inefect5 option:selected").val();
+    
+    
+    $('#titulouno').html('<h1 class="card-title tlt text-dark"  style="font-size: 2.4vw"> '+nombre+'</h1>');   
+    textille(val, cargo);   
+    
+});
+$('#Mostrar6').click(function(){
+
+    $('#titulouno').addClass('slideLeft');
+    
+       var nombre = $('#nombre6').val();
+       var cargo = $('#cargo6').val();
+
+       var txt = $("#inefect6 option:selected").text();
+       var val = $("#inefect6 option:selected").val();
+    
+    
+    $('#titulouno').html('<h1 class="card-title tlt text-dark"  style="font-size: 2.4vw"> '+nombre+'</h1>');   
+    textille(val, cargo);   
+    
+});
+$('#Mostrar7').click(function(){
+
+    $('#titulouno').addClass('slideLeft');
+    
+       var nombre = $('#nombre7').val();
+       var cargo = $('#cargo7').val();
+
+       var txt = $("#inefect7 option:selected").text();
+       var val = $("#inefect7 option:selected").val();
+    
+    
+    $('#titulouno').html('<h1 class="card-title tlt text-dark"  style="font-size: 2.4vw"> '+nombre+'</h1>');   
+    textille(val, cargo);   
+    
+});
+$('#Mostrar8').click(function(){
+
+    $('#titulouno').addClass('slideLeft');
+    
+       var nombre = $('#nombre8').val();
+       var cargo = $('#cargo8').val();
+
+       var txt = $("#inefect8 option:selected").text();
+       var val = $("#inefect8 option:selected").val();
+    
+    
+    $('#titulouno').html('<h1 class="card-title tlt text-dark"  style="font-size: 2.4vw"> '+nombre+'</h1>');   
+    textille(val, cargo);   
+    
+});
+$('#Mostrar9').click(function(){
+
+    $('#titulouno').addClass('slideLeft');
+    
+       var nombre = $('#nombre9').val();
+       var cargo = $('#cargo9').val();
+
+       var txt = $("#inefect9 option:selected").text();
+       var val = $("#inefect9 option:selected").val();
+    
+    
+    $('#titulouno').html('<h1 class="card-title tlt text-dark"  style="font-size: 2.4vw"> '+nombre+'</h1>');   
+    textille(val, cargo);   
+    
+});
+$('#Mostrar10').click(function(){
+
+    $('#titulouno').addClass('slideLeft');
+    
+       var nombre = $('#nombre10').val();
+       var cargo = $('#cargo10').val();
+
+       var txt = $("#inefect10 option:selected").text();
+       var val = $("#inefect10 option:selected").val();
+    
+    
+    $('#titulouno').html('<h1 class="card-title tlt text-dark"  style="font-size: 2.4vw"> '+nombre+'</h1>');   
+    textille(val, cargo);   
+    
+});
+$('#Mostrar11').click(function(){
+
+    $('#titulouno').addClass('slideLeft');
+    
+       var nombre = $('#nombre11').val();
+       var cargo = $('#cargo11').val();
+
+       var txt = $("#inefect11 option:selected").text();
+       var val = $("#inefect11 option:selected").val();
+    
+    
+    $('#titulouno').html('<h1 class="card-title tlt text-dark"  style="font-size: 2.4vw"> '+nombre+'</h1>');   
+    textille(val, cargo);   
+    
+});
+$('#Mostrar12').click(function(){
+
+    $('#titulouno').addClass('slideLeft');
+    
+       var nombre = $('#nombre12').val();
+       var cargo = $('#cargo12').val();
+
+       var txt = $("#inefect12 option:selected").text();
+       var val = $("#inefect12 option:selected").val();
+    
+    
+    $('#titulouno').html('<h1 class="card-title tlt text-dark"  style="font-size: 2.4vw"> '+nombre+'</h1>');   
+    textille(val, cargo);   
+    
+});
+
+$('#Ocultar').click(function(){
+    $('.tlt').hide(1000);
+    $('.tltl').hide(1000);  
+});
+$('#Ocultar2').click(function(){
+    $('.tlt').hide(1000);
+    $('.tltl').hide(1000);  
+});
+$('#Ocultar3').click(function(){
+    $('.tlt').hide(1000);
+    $('.tltl').hide(1000);  
+});
+$('#Ocultar4').click(function(){
+    $('.tlt').hide(1000);
+    $('.tltl').hide(1000);  
+});
+$('#Ocultar5').click(function(){
+    $('.tlt').hide(1000);
+    $('.tltl').hide(1000);  
+});
+$('#Ocultar6').click(function(){
+    $('.tlt').hide(1000);
+    $('.tltl').hide(1000);  
+});
+$('#Ocultar7').click(function(){
+    $('.tlt').hide(1000);
+    $('.tltl').hide(1000);  
+});
+$('#Ocultar8').click(function(){
+    $('.tlt').hide(1000);
+    $('.tltl').hide(1000);  
+});
+$('#Ocultar9').click(function(){
+    $('.tlt').hide(1000);
+    $('.tltl').hide(1000);  
+});
+$('#Ocultar10').click(function(){
+    $('.tlt').hide(1000);
+    $('.tltl').hide(1000);  
+});
+$('#Ocultar11').click(function(){
+    $('.tlt').hide(1000);
+    $('.tltl').hide(1000);  
+});
+$('#Ocultar12').click(function(){
+    $('.tlt').hide(1000);
+    $('.tltl').hide(1000);  
+});
+
+$("a[rel='pop-up']").click(function () {
+    var caracteristicas = "height=700,width=800,scrollTo,resizable=1,scrollbars=1,location=0";
+    nueva=window.open(this.href, 'Popup', caracteristicas);
+    return false;
+});
+
+
+
+
+function textille(val , cargo) {
+      
+  $('.tlt').textillate({
+    selector: '.texts',
+    loop: false,      
+    minDisplayTime: 2000,      
+    initialDelay: 0,      
+    autoStart: true,      
+    inEffects: [],      
+    outEffects: [ 'hinge' ],
+    in: {
+      effect: val,
+      delayScale: 1.5,
+      delay: 50,
+      sync: false,
+      shuffle: false,
+      reverse: false,
+      callback: function () {}
+    },
+    out: {
+      effect: 'hinge',
+      delayScale: 1.5,
+      delay: 50,
+      sync: false,
+      shuffle: false,
+      reverse: false,
+      callback: function () {}
+    },
+    callback: function () {},
+    type: 'char'
+  });
+
+setTimeout(
+    function() 
+    {
+        $('#titulodos').html('<h4 class="card-title tltl text-dark" style="font-size: 1.2vw" > '+cargo + '</h4> ');
+        $('.tltl').textillate({
+            selector: '.texts',
+            loop: false,
+            minDisplayTime: 2000,
+            initialDelay: 0,
+            autoStart: true,
+            inEffects: [],
+            outEffects: [ 'hinge' ],
+          
+            in: {
+              effect: val,
+              delayScale: 1.5,
+              delay: 50,
+              sync: false,
+              shuffle: false,
+              reverse: false,
+              callback: function () {}
+            },
+          
+            out: {
+              effect: 'hinge',
+              delayScale: 1.5,
+              delay: 50,
+              sync: false,
+              shuffle: false,
+              reverse: false,
+              callback: function () {}
+            },
+    
+            callback: function () {},
+            type: 'char'
+          });
+    }, 500);
+
+}
+
+
+
+
+
